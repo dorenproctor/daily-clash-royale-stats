889432489166982bd313b7ca54108e55b2d1a338
Doren Proctor <dorenproctor@gmail.com>
2020-08-07 00:38:29 -0700

    cards_to_query.json + change git stuff in ci + README

---
+++ .github/workflows/README.md
@@ -0,0 +1,3 @@
+This small project uses a python script to fetch stats about Clash Royale cards from [https://clashroyale.fandom.com](https://clashroyale.fandom.com) with BeatifulSoup. After scraping the stats, it turns it into JSON and stores it into a file called card_stats.json.
+
+This script runs daily using GitHub Actions to maintain up-to-date data.
--- fetch_cr_stats.py
+++ fetch_cr_stats.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import json
 from time import time
 
-card_names = ['Archers', 'Arrows', 'Baby_Dragon', 'Balloon', 'Bandit', 'Barbarian_Barrel', 'Barbarian_Hut', 'Barbarians', 'Bats', 'Battle_Healer', 'Battle_Ram', 'Bomb_Tower', 'Bomber', 'Bowler', 'Cannon', 'Cannon_Cart', 'Clone', 'Dark_Prince', 'Dart_Goblin', 'Earthquake', 'Electro_Dragon', 'Electro_Wizard', 'Elite_Barbarians', 'Elixir_Collector', 'Elixir_Golem', 'Executioner', 'Fire_Spirits', 'Fireball', 'Firecracker', 'Fisherman', 'Flying_Machine', 'Freeze', 'Furnace', 'Giant', 'Giant_Skeleton', 'Giant_Snowball', 'Goblin_Barrel', 'Goblin_Cage', 'Goblin_Gang', 'Goblin_Giant', 'Goblin_Hut', 'Goblins', 'Golem', 'Graveyard', 'Guards', 'Heal', 'Hog_Rider', 'Hunter', 'Ice_Golem', 'Ice_Spirit', 'Ice_Wizard', 'Inferno_Dragon', 'Inferno_Tower', 'Knight', 'Lava_Hound', 'Lightning', 'Lumberjack', 'Magic_Archer', 'Mega_Knight', 'Mega_Minion', 'Miner', 'Mini_P.E.K.K.A.', 'Minion_Horde', 'Minions', 'Mirror', 'Mortar', 'Musketeer', 'Night_Witch', 'P.E.K.K.A.', 'Poison', 'Prince', 'Princess', 'Rage', 'Ram_Rider', 'Rascals', 'Rocket', 'Royal_Ghost', 'Royal_Giant', 'Royal_Hogs', 'Royal_Recruits', 'Skeleton_Army', 'Skeleton_Barrel', 'Skeletons', 'Sparky', 'Spear_Goblins', 'Tesla', 'The_Log', 'Three_Musketeers', 'Tombstone', 'Tornado', 'Valkyrie', 'Wall_Breakers', 'Witch', 'Wizard', 'X-Bow', 'Zap', 'Zappies']
-
 def getText(elem):
     while elem.string == None: elem = next(elem.children)
     return elem.string.strip()
@@ -41,6 +39,10 @@ def parseSiteForStats(card_name, all_cards, errs):
     except Exception as e:
         print(card_name,e)
         errs.append('{}: {}'.format(card_name,e))
+
+
+with open('cards_to_query.json', 'r') as f:
+    card_names = json.load(f)
 
 all_cards = {}
 errs = []
